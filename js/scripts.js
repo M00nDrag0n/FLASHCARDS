66dbcb4e05b7ebcb9e608d481c4680ba19d8d6b8
Anise Lester and Duncan Robbins <anise@>
2019-08-13 16:22:37 -0700

    will add list and JS

---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+  $(document).ready(function() {
+    $(".clickable").click(function() {
+      $(".visible").toggle();
+      $(".invisible").toggle();
+
+  $("button#JavaScript").click(function() {
+    $("body").removeClass();
+    $("#JavaScript").toggle();
+  }
+  $("button#Operators").click(function() {
+    $("#Operators").toggle();
+
+  }
+  $("button#Variables").click(function() {
+    $("#Variables"").toggle();
+
+  }
+  $("button#V-n-c").click(function() {
+    $("#V-n-c").toggle();
+
+  }
+  $("button#Functions").click(function() {
+    $("#Functions").toggle();
+
+  }
+  $("button#Methods").click(function() {
+    $("#Methods").toggle();
+
+  }
+  $("button#Arguments").click(function() {
+    $("#Arguments").toggle();
+  }
+  $("button#Parameters").click(function() {
+    $("#Parameters").toggle();
+  }
+  $("button#Return").click(function() {
+    $("#Return").toggle();
+  }
+  $("button#Chaining-Methods").click(function() {
+    $("#Chaining-Methods").toggle();
+  }
+  $("button#Strings").click(function() {
+    $("#Strings").toggle();
+  }
+  $("button#Booleans").click(function() {
+    $("#Booleans").toggle();
+  }
+  $("button#Undefined").click(function() {
+    $("#Undefined").toggle();
+  }
+  $("button#NaN").click(function() {
+    $("#NaN").toggle();
+  }
+  $("button#Escape").click(function() {
+    $("#Escape").toggle();
+  }
+  $("button#Alert").click(function() {
+    $("#Alert").toggle();
+  }
+  $("button#Comments").click(function() {
+    $("#Comments").toggle();
+  }
+  $("button#jQuery").click(function() {
+    $("#jQuery").toggle();
+
+  }
+  $("button#Attributes").click(function() {
+    $("#Attributes").toggle();
+  }
+
+
+
+
+
+
+
+
+
+
+)
+
+
+})
